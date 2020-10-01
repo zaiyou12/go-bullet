8a70eccbfbb9a2d23abdca022ee6a2a199b646ca
Aaron So <zaiyou12@gmail.com>
2020-10-01 15:36:04 +0900

    Add: Mover and Rotator

---
+++ RadomRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadomRotator : MonoBehaviour
+{
+    public float tumble;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
